e33fd6d073e0cce6c0193301725e4e0f51c704bc
Justin Kuan <32026134+jkuan1@users.noreply.github.com>
2025-09-07 23:26:10 -0700

    Update 0146-LRU-cache.js - double link list implementation

--- javascript/0146-LRU-cache.js
+++ javascript/0146-LRU-cache.js
@@ -41,3 +41,83 @@ LRUCache.prototype.put = function(key, value) {
  * var param_1 = obj.get(key)
  * obj.put(key,value)
  */
+
+
+
+// NON MAP IMPLEMENTATION (DOUBLE LINKED LIST)
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.capacity = capacity
+    this.nodeMap = {}
+    this.size = 0
+
+    this.startSentinal = new LinkedNode(null, null, null, null)
+    this.endSentinal = new LinkedNode(null, null, null, this.startSentinal) 
+    this.startSentinal.next = this.endSentinal
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    const node = this.nodeMap[key]
+    if (!node) return -1
+
+    node.next.prev = node.prev
+    node.prev.next = node.next
+
+    this.endSentinal.prev.next = node
+    node.prev = this.endSentinal.prev
+
+    this.endSentinal.prev = node
+    node.next = this.endSentinal
+
+    return node.value
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+
+    const node = this.nodeMap[key];
+    if (node) {
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        delete this.nodeMap[key]
+        this.size -= 1
+    }
+
+    if (this.size == this.capacity) {
+        const nodeToDelete = this.startSentinal.next
+        nodeToDelete.next.prev = this.startSentinal
+        this.startSentinal.next = nodeToDelete.next
+        delete this.nodeMap[nodeToDelete.key]
+        this.size -= 1
+    }
+
+    const newNode = new LinkedNode(key, value, this.endSentinal, this.endSentinal.prev)
+    this.endSentinal.prev.next = newNode
+    this.endSentinal.prev = newNode
+    this.nodeMap[key] = newNode
+    this.size += 1
+};
+
+var LinkedNode = function(key, value, next, prev) {
+    this.next = next
+    this.prev = prev
+    this.key = key
+    this.value = value
+}
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
